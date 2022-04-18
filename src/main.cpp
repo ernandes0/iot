aed22c034202742e957f07936b8008439228c445
Ern4nde5 <ernandessluiz@utic.pe.senac.br>
2022-04-18 09:41:49 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <Ultrasonic.h>
+#include <CircularBuffer.h>
+
+#define Ptrig   2 //atribuindo o pino 2 ao trigger do sensor
+#define Pecho   3 //atribuindo o pino 3 ao echo do sensor
+#define Pled    LED_BUILTIN //atribuindo o pino 8 ao led builtin
+CircularBuffer<int, 60> buffer;
+
+int dist;
+Ultrasonic ultrasonic(Ptrig, Pecho);
+
+void teclado(){
+  char tecla;
+  tecla = Serial.read();
+  if (tecla == 'L'){
+    Serial.println("As ultimas medições foram:");
+    int last;
+    for (int i = buffer.size(); i > (buffer.size() - 10); i--) {
+			last = buffer[i];
+      Serial.println(last);
+    }
+  }
+}
+
+void setup(){
+  pinMode(Pled, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  dist = ultrasonic.read();
+  buffer.push(dist);
+
+  if (dist != 0){
+    digitalWrite(Pled, HIGH);
+    delay(200);
+    digitalWrite(Pled, LOW);
+  }
+  teclado();
+  Serial.println("Aperte a tecla 'L' para ter acesso as ultimas medições");
+  delay(10000);
+}
